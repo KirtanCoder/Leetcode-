a7fa213682f9f639903d9ed54a45a01e1b54dd0f
KirtanCoder <kirtankrpc@gmail.com>
2026-09-15 12:59:43 +0530

    Time: 0 ms (100%), Space: 117.9 MB (93.87%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,40 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-
-        // 1. Find middle
+        //find middle node
         ListNode* slow = head;
         ListNode* fast = head;
-
-        while (fast != NULL && fast->next != NULL) {
+        while(fast!=NULL && fast->next!=NULL){
+            fast= fast->next->next;
             slow = slow->next;
-            fast = fast->next->next;
         }
+ // 2. Reverse second half
+ListNode * prev = NULL;
+ListNode* curr = slow;
 
-        // 2. Reverse second half
-        ListNode* prev = NULL;
-        ListNode* curr = slow;
+while(curr!=NULL){
+    ListNode * next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+}
 
-        while (curr != NULL) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
+//check pallindrome
 
-        // 3. Compare first half and reversed second half
-        ListNode* left = head;
-        ListNode* right = prev;
+ListNode *left =head;
+ListNode * right = prev;
 
-        while (right != NULL) {
-            if (left->val != right->val) {
-                return false;
-            }
+    
+while(right!=NULL){
+    if(left->val!=right->val){
 
-            left = left->next;
-            right = right->next;
-        }
+        return false;
+    }
+    left = left->next;
+    right = right->next;
+}
 
-        return true;
+
+return true;
     }
 };
