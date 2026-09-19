b7b6932a92165bbd804dd28a0a22e1b3b0525e37
KirtanCoder <kirtankrpc@gmail.com>
2026-09-19 13:44:26 +0530

    Time: 4 ms (85.88%), Space: 45.1 MB (11.93%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+
+        int i=0;
+        int j=0;
+        int ans =0;
+
+
+        while(i<g.size() &&  j<s.size()){
+           if( s[j]>= g[i]){
+            //child satisfy
+
+            ans++;
+            i++;
+            j++;
+           }
+           else {
+            //cookies too small
+            j++;
+           }
+        }
+
+        return ans;
+    }
+};
